676fe2d260d0d2d6ded35b0cec3a6d7cd5f6af31
roalcantara <rogerio.alcantara@gmail.com>
2025-08-15 18:40:48 -0300

    ref(nvim/autocmds): Refactor and enhance autocmd configuration
    - Consolidated autocmd definitions for better organization and
      readability.
    - Retained essential functionality while removing redundant code.
    - Improved handling of filetype-specific settings and added
      notifications for wrap and spell features.
    - Ensured proper setup for LSP capabilities on attach events.

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,252 +1,249 @@
 -- https://lazyvim.org/configuration/general#auto-commands
 require('core/vi/au/setup_au_async')({
-  modules = {
-    augroup = { path = 'core/vi/au/aug' },
-    autocmd = { path = 'core/vi/au/au' },
+  checktime = {
+    {
+      event = { 'FocusGained', 'TermClose', 'TermLeave' },
+      opts = {
+        callback = function()
+          if vim.o.buftype ~= 'nofile' then
+            vim.cmd('checktime')
+          end
+        end,
+        desc = 'Check if we need to reload the file when it changed',
+      },
+    }
   },
-  autocmds = {
-    checktime = {
-      {
-        event = { 'FocusGained', 'TermClose', 'TermLeave' },
-        opts = {
-          callback = function()
-            if vim.o.buftype ~= 'nofile' then
-              vim.cmd('checktime')
-            end
-          end,
-          desc = 'Check if we need to reload the file when it changed',
-        },
-      }
-    },
-    highlight_yank = {
-      {
-        event = 'TextYankPost',
-        opts = {
-          callback = function()
-            (vim.hl or vim.highlight).on_yank()
-          end,
-          desc = 'Highlight on yank',
-        },
-      }
-    },
-    resize_splits = {
-      {
-        event = 'VimResized',
-        opts = {
-          callback = function()
-            local current_tab = vim.fn.tabpagenr()
-            vim.cmd('tabdo wincmd =')
-            vim.cmd('tabnext ' .. current_tab)
-          end,
-          desc = 'Resize splits when window is resized',
-        },
-      }
-    },
-    last_loc = {
-      {
-        event = 'BufReadPost',
-        opts = {
-          callback = function(event)
-            local exclude = { 'gitcommit' }
-            local buf = event.buf
-            if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].neovim_last_loc then
-              return
-            end
-            vim.b[buf].neovim_last_loc = true
-            local mark = vim.api.nvim_buf_get_mark(buf, "'")
-            local lcount = vim.api.nvim_buf_line_count(buf)
-            if mark[1] > 0 and mark[1] <= lcount then
-              pcall(vim.api.nvim_win_set_cursor, 0, mark)
-            end
-          end,
-          desc = 'Go to last location when opening a buffer',
-        },
-      }
-    },
-    man_unlisted = {
-      {
-        event = 'FileType',
-        opts = {
-          pattern = { 'man' },
-          callback = function(event)
-            vim.bo[event.buf].buflisted = false
-          end,
-          desc = 'Make it easier to close man-files when opened inline',
-        },
-      }
-    },
-    wrap_spell = {
-      {
-        event = 'FileType',
-        opts = {
-          pattern = { 'text', 'plaintex', 'typst', 'gitcommit', 'markdown' },
-          callback = function()
-            vim.opt_local.wrap = true
-            vim.opt_local.spell = true
-          end,
-          desc = 'Enable wrap and check for spell in text filetypes',
-        },
-      }
-    },
-    json_conceal = {
-      {
-        event = 'FileType',
-        opts = {
-          pattern = { 'json', 'jsonc', 'json5' },
-          callback = function()
-            vim.opt_local.conceallevel = 0
-          end,
-          desc = 'Disable conceallevel for json files',
-        },
-      }
-    },
-    auto_create_dir = {
-      {
-        event = 'BufWritePre',
-        opts = {
-          callback = function(event)
-            if event.match:match('^%w%w+:[\\/][\\/]') then
-              return
-            end
-            local file = vim.uv.fs_realpath(event.match) or event.match
-            vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
-          end,
-          desc = 'Auto create directory when saving a file, in case some intermediate directory does not exist'
-        }
-      }
-    },
-    filetypedetect = {
-      {
-        event = 'BufRead',
-        opts = {
-          pattern = { '*.yml' },
-          command = 'set filetype=yaml',
-          desc = 'Setup filetype=yaml for files ended with .yml',
-        },
+  highlight_yank = {
+    {
+      event = 'TextYankPost',
+      opts = {
+        callback = function()
+          (vim.hl or vim.highlight).on_yank()
+        end,
+        desc = 'Highlight on yank',
       },
-      {
-        event = 'BufNewFile',
-        opts = {
-          pattern = { '*.yml' },
-          command = 'set filetype=yaml',
-          desc = 'Setup filetype=yaml for files ended with .yml',
-        },
+    }
+  },
+  resize_splits = {
+    {
+      event = 'VimResized',
+      opts = {
+        callback = function()
+          local current_tab = vim.fn.tabpagenr()
+          vim.cmd('tabdo wincmd =')
+          vim.cmd('tabnext ' .. current_tab)
+        end,
+        desc = 'Resize splits when window is resized',
+      },
+    }
+  },
+  last_loc = {
+    {
+      event = 'BufReadPost',
+      opts = {
+        callback = function(event)
+          local exclude = { 'gitcommit' }
+          local buf = event.buf
+          if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].neovim_last_loc then
+            return
+          end
+          vim.b[buf].neovim_last_loc = true
+          local mark = vim.api.nvim_buf_get_mark(buf, "'")
+          local lcount = vim.api.nvim_buf_line_count(buf)
+          if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+          end
+        end,
+        desc = 'Go to last location when opening a buffer',
+      },
+    }
+  },
+  man_unlisted = {
+    {
+      event = 'FileType',
+      opts = {
+        pattern = { 'man' },
+        callback = function(event)
+          vim.bo[event.buf].buflisted = false
+        end,
+        desc = 'Make it easier to close man-files when opened inline',
+      },
+    }
+  },
+  wrap_spell = {
+    {
+      event = 'FileType',
+      opts = {
+        pattern = { 'text', 'plaintex', 'typst', 'gitcommit', 'markdown' },
+        callback = function(event)
+          local buf = event.buf
+          vim.opt_local.wrap = true
+          vim.opt_local.spell = true
+          vim.notify("wrap and spell enabled for " .. vim.bo[buf].filetype, vim.log.levels.INFO, {
+            title = "wrap and spell",
+            timeout = 3000,
+          })
+        end,
+        desc = 'Enable wrap and check for spell in text filetypes',
+      },
+    }
+  },
+  json_conceal = {
+    {
+      event = 'FileType',
+      opts = {
+        pattern = { 'json', 'jsonc', 'json5' },
+        callback = function()
+          vim.opt_local.conceallevel = 0
+        end,
+        desc = 'Disable conceallevel for json files',
+      },
+    }
+  },
+  auto_create_dir = {
+    {
+      event = 'BufWritePre',
+      opts = {
+        callback = function(event)
+          if event.match:match('^%w%w+:[\\/][\\/]') then
+            return
+          end
+          local file = vim.uv.fs_realpath(event.match) or event.match
+          vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+        end,
+        desc = 'Auto create directory when saving a file, in case some intermediate directory does not exist'
+      }
+    }
+  },
+  filetypedetect = {
+    {
+      event = 'BufRead',
+      opts = {
+        pattern = { '*.yml' },
+        command = 'set filetype=yaml',
+        desc = 'Setup filetype=yaml for files ended with .yml',
       },
     },
-    close_with_q_or_esc = {
-      {
-        event = 'FileType',
-        opts = {
-          pattern = {
-            'aerial',
-            'checkhealth',
-            'dbout',
-            'DressingSelect',
-            'floaterm',
-            'gitsigns-blame',
-            'grug-far',
-            'help',
-            'Jaq',
-            'lazy',
-            'lir',
-            'lsp-installer',
-            'lspinfo',
-            'LspsagaCodeAction',
-            'LspsagaDiagnostic',
-            'LspsagaFinder',
-            'LspsagaFloaterm',
-            'LspsagaHover',
-            'LspsagaRename',
-            'LspsagaSignatureHelp',
-            'LspSignatureHelp',
-            'man',
-            'markdown',
-            'neotest-output-panel',
-            'neotest-output',
-            'neotest-summary',
-            'noice',
-            'notify',
-            'null-ls-info',
-            'PlenaryTestPopup',
-            'qf',
-            'snacks_notif_history',
-            'snacks_notif_log',
-            'snacks_notif',
-            'snacks_win',
-            'snacks_picker_list',
-            'spectre_panel',
-            'startuptime',
-            'trouble',
-            'Trouble',
-            'TroubleToggle',
-            'tsplayground',
-            'unix',
-            "checkhealth",
-            "dbout",
-            "gitsigns-blame",
-            "grug-far",
-            "help",
-            "lspinfo",
-            "neotest-output-panel",
-            "neotest-output",
-            "neotest-summary",
-            "notify",
-            "PlenaryTestPopup",
-            "qf",
-            "spectre_panel",
-            "startuptime",
-            "tsplayground",
-          },
-          callback = function(event)
-            vim.bo[event.buf].buflisted = false
-            vim.schedule(function()
-              vim.keymap.set('n', 'q', function()
-                vim.cmd('close')
-                pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
-              end, {
-                buffer = event.buf,
-                silent = true,
-                desc = 'Quit buffer (q)',
-              })
-              vim.keymap.set('n', '<Esc>', function()
-                vim.cmd('close')
-                pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
-              end, {
-                buffer = event.buf,
-                silent = true,
-                desc = 'Quit buffer (<Esc>)',
-              })
-            end)
-          end,
-          desc = 'Close buffer with <Esc>',
-        },
+    {
+      event = 'BufNewFile',
+      opts = {
+        pattern = { '*.yml' },
+        command = 'set filetype=yaml',
+        desc = 'Setup filetype=yaml for files ended with .yml',
       },
-    },
-    on_open_term = {
-      {
-        event = 'TermOpen',
-        opts = {
-          callback = function()
-            vim.opt.number = false
-            vim.opt.relativenumber = false
-            vim.cmd('startinsert')
-          end,
-        },
-      },
-    },
-    on_lsp_attach_set_capabilities = {
-      {
-        event = 'LspAttach',
-        opts = {
-          callback = function(event)
-            local client = vim.lsp.get_client_by_id(event.data.client_id)
-            if type(client) ~= nil and vim.api.nvim_buf_is_valid(event.buf) then
-              require('core/vi/lsp/features').setup_capabilities(client, event.buf, event)
-            end
-          end,
-          desc = 'LSP Attach: Setup autocommands and settings for LSP feature capabilities',
-        },
-      }
     },
   },
+  close_with_q_or_esc = {
+    {
+      event = 'FileType',
+      opts = {
+        pattern = {
+          'aerial',
+          'checkhealth',
+          'dbout',
+          'DressingSelect',
+          'floaterm',
+          'gitsigns-blame',
+          'grug-far',
+          'help',
+          'Jaq',
+          'lazy',
+          'lir',
+          'lsp-installer',
+          'lspinfo',
+          'LspsagaCodeAction',
+          'LspsagaDiagnostic',
+          'LspsagaFinder',
+          'LspsagaFloaterm',
+          'LspsagaHover',
+          'LspsagaRename',
+          'LspsagaSignatureHelp',
+          'LspSignatureHelp',
+          'man',
+          'markdown',
+          'neotest-output-panel',
+          'neotest-output',
+          'neotest-summary',
+          'noice',
+          'notify',
+          'null-ls-info',
+          'PlenaryTestPopup',
+          'qf',
+          'snacks_notif_history',
+          'snacks_notif_log',
+          'snacks_notif',
+          'snacks_win',
+          'snacks_picker_list',
+          'spectre_panel',
+          'startuptime',
+          'trouble',
+          'Trouble',
+          'TroubleToggle',
+          'tsplayground',
+          'unix',
+          "checkhealth",
+          "dbout",
+          "gitsigns-blame",
+          "grug-far",
+          "help",
+          "lspinfo",
+          "neotest-output-panel",
+          "neotest-output",
+          "neotest-summary",
+          "notify",
+          "PlenaryTestPopup",
+          "qf",
+          "spectre_panel",
+          "startuptime",
+          "tsplayground",
+        },
+        callback = function(event)
+          vim.bo[event.buf].buflisted = false
+          vim.keymap.set('n', 'q', function()
+            vim.cmd('close')
+            pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+          end, {
+            buffer = event.buf,
+            silent = true,
+            desc = 'Quit buffer (q)',
+          })
+          vim.keymap.set('n', '<Esc>', function()
+            vim.cmd('close')
+            pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+          end, {
+            buffer = event.buf,
+            silent = true,
+            desc = 'Quit buffer (<Esc>)',
+          })
+        end,
+        desc = 'Close buffer with <Esc>',
+      },
+    },
+  },
+  on_open_term = {
+    {
+      event = 'TermOpen',
+      opts = {
+        callback = function()
+          vim.opt.number = false
+          vim.opt.relativenumber = false
+          vim.cmd('startinsert')
+        end,
+      },
+    },
+  },
+  on_lsp_attach_set_capabilities = {
+    {
+      event = 'LspAttach',
+      opts = {
+        callback = function(event)
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if type(client) ~= nil and vim.api.nvim_buf_is_valid(event.buf) then
+            require('core/vi/lsp/features').setup_capabilities(client, event.buf, event)
+          end
+        end,
+        desc = 'LSP Attach: Setup autocommands and settings for LSP feature capabilities',
+      },
+    }
+  }
 })
