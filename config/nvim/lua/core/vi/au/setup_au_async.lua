ff71b0d5417e57a58ee706a6a6f2c8b3f5675ba2
roalcantara <rogerio.alcantara@gmail.com>
2025-08-15 18:40:48 -0300

    ref(nvim/autocmds): Refactor and enhance autocmd configuration
    - Consolidated autocmd definitions for better organization and
      readability.
    - Retained essential functionality while removing redundant code.
    - Improved handling of filetype-specific settings and added
      notifications for wrap and spell features.
    - Ensured proper setup for LSP capabilities on attach events.

--- config/nvim/lua/core/vi/au/setup_au_async.lua
+++ config/nvim/lua/core/vi/au/setup_au_async.lua
@@ -31,46 +31,15 @@ local h = {
     return module
   end,
 }
--- local function create_single_user_command(info)
---   local name, module_path, opts, action = info.cmd, info.module.path, info.opts, info.action
-
---   local module = h.get_module(module_path)
---   if not module then
---     Snacks.notify.error('Failed to load module: ' .. module_path)
---     return false
---   end
-
---   local command = h.get_command(module, action)
---   if type(command) ~= 'function' and type(command) ~= 'string' then
---     Snacks.notify.error('Failed to load command: `' ..
---       name .. '`. Module is not a function or action is not defined!')
---     return false
---   end
-
---   return vim.api.nvim_create_user_command(name, command, opts or {})
--- end
-
-h.load_modules = function(options)
-  local mods = {}
-  for mod_name, mod in pairs(options) do
-    mods[mod_name] = h.get_module(mod.path)
-  end
-  return mods
-end
 
 local function setup_au_async(options)
   --- @async
   local init_co = coroutine.create(function()
-    local mods = h.load_modules(options.modules)
-    local augroup = mods.augroup
-    local autocmd = mods.autocmd
     for augroup_name, autocommands in pairs(options.autocmds) do
+      local group = vim.api.nvim_create_augroup("neovim_custom_" .. augroup_name, { clear = true })
       for _, autocommand in ipairs(autocommands) do
-        autocmd(
-          autocommand.event,
-          vim.tbl_extend('force', autocommand.opts, {
-            group = augroup(augroup_name),
-          })
+        vim.api.nvim_create_autocmd(autocommand.event,
+          vim.tbl_deep_extend('force', autocommand.opts or {}, { group = group })
         )
       end
     end
