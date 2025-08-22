29017bf5e30f92b663f06ab4a3c921513d91c1c0
roalcantara <rogerio.alcantara@gmail.com>
2025-08-22 09:52:21 -0300

    chore(nvim): Update formatter configuration and dependencies
    - Updated `lazy-lock.json` to reflect new commits for several plugins,
      like `aerial.nvim`, `conform.nvim`, `copilot.lua`, `nvim-lspconfig`.
    
    - Enhanced `formater.lua` to conditionally load formatters based on the
      availability of required tools, improving flexibility and error
      handling during formatting operations.
    
    More: https://github.com/stevearc/conform.nvim/blob/master/doc/conform.txt

--- config/nvim/lua/plugins/formater.lua
+++ config/nvim/lua/plugins/formater.lua
@@ -1,3 +1,17 @@
+local has = require('core/vi/paths').has
+local function get_available_formatters(lang, bufnr, possible_formatters, opts)
+  local formatters = {}
+  opts = opts or {}
+  if has(lang) then
+    for _, formatter in ipairs(possible_formatters) do
+      if require('conform').get_formatter_info(formatter, bufnr).available then
+        table.insert(formatters, formatter)
+      end
+    end
+  end
+  return vim.tbl_deep_extend('force', formatters, opts)
+end
+
 return {
   -- Lightweight yet powerful formatter plugin for Neovim
   -- https://github.com/stevearc/conform.nvim
@@ -11,23 +25,44 @@ return {
       -- https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
       formatters_by_ft = {
         lua = { 'stylua' },
-        go = { 'goimports', 'golines', 'gofmt' },
         -- Conform will run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        go = function(bufnr)
+          return get_available_formatters('go', bufnr, { 'goimports', 'golines', 'gofmt' })
+        end,
+        python = function(bufnr)
+          return get_available_formatters('python', bufnr, { 'ruff' })
+        end,
         -- You can customize some of the format options for the filetype (:help conform.format)
-        rust = { 'rustfmt' },
+        rust = function(bufnr)
+          return get_available_formatters('rust', bufnr, { 'rustfmt' })
+        end,
         -- Conform will run the first available formatter
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = function(bufnr)
+          return get_available_formatters('node', bufnr, { 'prettierd', 'prettier' }, { stop_after_first = true })
+        end,
+        typescript = function(bufnr)
+          return get_available_formatters('node', bufnr, { 'prettierd', 'prettier' }, { stop_after_first = true })
+        end,
+      },
+      -- Set this to change the default values when calling conform.format()
+      -- This will also affect the default values for format_on_save/format_after_save
+      default_format_opts = {
+        lsp_format = 'fallback', -- Configure if and when LSP should be used for formatting. Defaults to "never".
       },
       -- Create autocmd (on BufWritePre) to format on save automatically
       -- https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatopts-callback
       -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#autoformat-with-extra-features
       format_on_save = {
         -- These options will be passed to conform.format()
-        timeout_ms = 500,        -- Time in milliseconds to block for formatting. Defaults to 1000. No effect if async = true.
-        lsp_format = 'fallback', -- Configure if and when LSP should be used for formatting. Defaults to "never".
+        timeout_ms = 500, -- Time in milliseconds to block for formatting. Defaults to 1000. No effect if async = true.
       },
+      -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+      log_level = vim.log.levels.ERROR,
+      -- Conform will notify you when a formatter errors
+      notify_on_error = true,
+      -- Conform will notify you when no formatters are available for the buffer
+      notify_no_formatters = true,
+      -- Custom formatters and overrides for built-in formatters
       formatters = {
         -- All formatters can be customized
         -- https://github.com/stevearc/conform.nvim/blob/master/README.md#customizing-formatters
@@ -71,6 +106,6 @@ return {
         --   }
         -- }
       },
-    }
+    },
   },
 }
