aa7548491c7accb804118850b0d8e4e339b13c31
roalcantara <rogerio.alcantara@gmail.com>
2025-09-20 23:09:09 -0300

    feat(nvim): Add toggle key mappings
    For comments, treesitter context and LSP logging.

--- config/nvim/lua/core/vi/au/setup_au_async.lua
+++ config/nvim/lua/core/vi/au/setup_au_async.lua
@@ -40,8 +40,4 @@ end
 
 -- Autocmds are automatically loaded on the VeryLazy event
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
-local function setup_autocommands_async(options)
-  return create_autocmds(options)
-end
-
-return setup_autocommands_async
+return create_autocmds
--- config/nvim/lua/core/vi/maps/set_keymaps.lua
+++ config/nvim/lua/core/vi/maps/set_keymaps.lua
@@ -584,16 +584,18 @@ local function set_keymaps(fn)
     },
     -- setup
     {
-      -- nvim 0.10.0 has builtin support for commenting (:h commenting)
-      -- NATIVE COMMENTS (https://neovim.io/doc/user/various.html#commenting)
-      -- But doesn't work when remapping with `vim.keymap.set` so we call directly...
-      -- gc<motion> must to be used manually
-      toggle_comments_mappings = function(key)
-        vim.cmd('nmap ' .. key .. ' gcc')
-        vim.cmd('imap ' .. key .. ' <C-O>gcc')
-        vim.cmd('vmap ' .. key .. ' gc')
-        vim.cmd('omap ' .. key .. ' gc')
-        vim.cmd('xmap ' .. key .. ' gc')
+      map_toggles = function(mappings)
+        for key, mapping in pairs(mappings) do
+          if type(mapping) == 'table' then
+            Snacks.toggle({
+              name = mapping.desc,
+              get = mapping.get,
+              set = mapping.set,
+            }):map(key)
+          elseif type(mapping) == 'function' then
+            mapping(key)
+          end
+        end
       end,
     }
   ))
